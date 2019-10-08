c1e4284bf0f3c772d1bfd77beef4b53dba721100
popclaudia <popclaudia97@gmail.com>
2019-10-08 15:08:45 +0300

    first commit

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApp.Models;
+
+namespace MovieApp.Data
+{
+    public class MvcMovieContext:DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options) { }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
